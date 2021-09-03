b3287a2fe5f7c3032377d6198ad50ea6e015dd90
TulaMagar <36879268+IshkMagar@users.noreply.github.com>
2021-09-02 22:43:21 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -148,7 +148,7 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'stmp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pubgmobile0918@gmail.com'
